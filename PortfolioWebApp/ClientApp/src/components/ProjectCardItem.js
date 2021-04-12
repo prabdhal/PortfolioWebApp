2bec88275f0a479482ab6e2877129b98cb9262ad
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2021-04-12 14:19:35 -0400

    Update project components and styling.

--- PortfolioWebApp/ClientApp/src/components/ProjectCardItem.js
+++ PortfolioWebApp/ClientApp/src/components/ProjectCardItem.js
@@ -5,28 +5,37 @@ import './Project.css';
 const ProjectCardItem = (props) => {
   return (
     <div className="project-card-container">
-        <div className="project-image-container">
-          <img src={props.src} alt={props.alt} />
-        </div>
-        <div className="project-details-container">
-          <h3>{props.title}</h3>
-          <p>{props.details}</p>
-          <div className="project-button-container">
-            <a
-              className="btn btn-primary mr-1"
+      <div className="project-image-container">
+        <img src={props.src} alt={props.alt} />
+      </div>
+      <div className="project-details-container">
+        <h3>{props.title}</h3>
+        <p>{props.details}</p>
+        <details>
+          <summary>Features</summary>
+          <ul>
+            <li>{props.pointOne}</li> 
+            <li>{props.pointTwo}</li>
+            <li>{props.pointThree}</li>
+            <li>{props.pointFour}</li>
+          </ul>
+        </details>
+        <div className="project-button-container">
+          <a
+            className="btn btn-primary mr-1"
             href={props.projectLink}
             target="_blank"
-            >
-              View Project
+          >
+            View Project
             </a>
-            <a
-              className="btn btn-secondary ml-1"
+          <a
+            className="btn btn-secondary ml-1"
             href={props.githubLink}
             target="_blank"
-            >
-              View GitHub
+          >
+            View GitHub
             </a>
-          </div>
+        </div>
       </div>
     </div>
   );
--- PortfolioWebApp/ClientApp/src/components/ProjectCards.js
+++ PortfolioWebApp/ClientApp/src/components/ProjectCards.js
@@ -9,7 +9,11 @@ const ProjectCards = () => {
         src={"./images/blog-simple-website.PNG"}
         alt={"BlogSimple project image"}
         title={"BlogSimple"}
-        details={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+        details={"BlogSimple is a blog posting website where users can sign up and easily post blogs. Users may also view blogs posted by other authors and choose to comment on their blog posts."}
+        pointOne={"Implemented repository design pattern to remove code redundancy and increase code maintainability and scalability."}
+        pointTwo={"Integrated an email delivery service called SendGrid, to be able to send users email and password confirmation emails."}
+        pointThree={"Implemented CRUD operations using .NET 5 MVC with entity framework and SQL Server as the database to store users, posts and comments."}
+        pointFour={"Deployed via azure using azure SQL database to store data and secrets key vault to store sensitive information."}
         projectLink={"https://blogsimple.azurewebsites.net/"}
         githubLink={"https://github.com/prabdhal/BlogSimple"}
       />
@@ -17,7 +21,11 @@ const ProjectCards = () => {
         src={"./images/destxplor-website.PNG"}
         alt={"DestXplor project image"}
         title={"DestXplor"}
-        details={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+        details={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."}
+        pointOne={"Implemented repository design pattern to remove code redundancy and increase code maintainability and scalability."}
+        pointTwo={"Integrated an email delivery service called SendGrid, to be able to send users email and password confirmation emails."}
+        pointThree={"Implemented CRUD operations using .NET 5 MVC with entity framework and SQL Server as the database to store users, posts and comments."}
+        pointFour={"Deployed via azure using azure SQL database to store data and secrets key vault to store sensitive information."}
         projectLink={"https://destxplor.azurewebsites.net/"}
         githubLink={"https://github.com/prabdhal/DestXplor"}
       />
@@ -26,6 +34,10 @@ const ProjectCards = () => {
         alt={"tower defence game project image"}
         title={"Tower Defence - The Enemy Wave"}
         details={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+        pointOne={"Implemented repository design pattern to remove code redundancy and increase code maintainability and scalability."}
+        pointTwo={"Integrated an email delivery service called SendGrid, to be able to send users email and password confirmation emails."}
+        pointThree={"Implemented CRUD operations using .NET 5 MVC with entity framework and SQL Server as the database to store users, posts and comments."}
+        pointFour={"Deployed via azure using azure SQL database to store data and secrets key vault to store sensitive information."}
         projectLink={"http://prabdhaliwal.herokuapp.com/tower-defence-3d-enemy-wave"}
         githubLink={"https://github.com/prabdhal/Tower-Defence-Game-Enemy-Wave"}
       />
