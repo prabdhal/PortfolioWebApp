5c6569a3b865751d09cd696a307ecc2526f8dec5
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2021-04-14 15:58:52 -0400

    Update Navbar hover effect.

--- PortfolioWebApp/ClientApp/src/components/Navbar.js
+++ PortfolioWebApp/ClientApp/src/components/Navbar.js
@@ -1,24 +1,64 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
 
 import './Navbar.css';
 
 const Navbar = () => {
+
+  const [showHome, setShowHome] = useState(false);
+  const [showProject, setShowProject] = useState(false);
+  const [showContact, setShowContact] = useState(false);
+
+  const hoverEffect = () => {
+
+  }
+
+  const underlineClassHome = (show) => {
+    if (show) {
+      return ("underline show");
+    }
+    return ("underline");
+  }
+  const underlineClassProject = (show) => {
+    if (show) {
+      return ("underline show");
+    }
+    return ("underline");
+  }
+  const underlineClassContact = (show) => {
+    if (show) {
+      return ("underline show");
+    }
+    return ("underline");
+  }
+
   return (
     <>
       <nav>
         <div><a href="/">Prab</a></div>
         <ul>
           <li>
-            <a href="/">Home</a>
-            <div className="underline"></div>
+            <a
+              href="/"
+              onMouseEnter={() => setShowHome(true)}
+              onMouseLeave={() => setShowHome(false)}
+            >Home</a>
+            <div className={underlineClassHome(showHome)}></div>
           </li>
           <li>
-            <a href="/projects">Projects</a>
-            <div className="underline"></div>
+            <a
+              href="/projects"
+              onMouseEnter={() => setShowProject(true)}
+              onMouseLeave={() => setShowProject(false)}
+            >Projects</a>
+            <div className={underlineClassProject(showProject)}></div>
           </li>
           <li>
-            <a href="/contact">Contact</a>
-            <div className="underline"></div>
+            <a
+              href="/contact"
+              onMouseEnter={() => setShowContact(true)}
+              onMouseLeave={() => setShowContact(false)}
+            >Contact</a>
+            <div className={underlineClassContact(showContact)}></div>
           </li>
         </ul>
       </nav>
